37e655cfef359ac8b173f297c0d0a53dd3e37d81
omaresmaeel <omaresmael590@gmail.com>
2023-04-04 23:37:54 +0200

    round the price result

--- app/Models/Meal.php
+++ app/Models/Meal.php
@@ -24,7 +24,7 @@ class Meal extends Model
     protected function discountedPrice(): Attribute
     {
         return Attribute::make(
-            get: fn () => $this->price - ($this->price * ($this->discount / 100)),
+            get: fn () => round($this->price - ($this->price * ($this->discount / 100))),
         );
     }
 
--- tests/Feature/MealTest.php
+++ tests/Feature/MealTest.php
@@ -14,16 +14,18 @@ beforeEach(function () {
 
 it('gets all meals with pagination', function () {
     $response = $this->getJson(route('meal.index'));
-    $this->assertEquals('meals retrieved', $response->json('message'));
 
+    $response->assertJson([
+        'message' => 'meals retrieved',
+    ]);
     $this->assertCount(10, $response->json('data'));
     $this->assertTrue(array_key_exists('links', $response->json()));
     $this->assertTrue(array_key_exists('meta', $response->json()));
     $this->assertEquals(Meal::PAGINATION_COUNT, $response->json('meta.per_page'));
 });
 
-it('calcutates discounted price', function () {
-    $dicountedPrice = $this->meal->price - ($this->meal->price * ($this->meal->discount / 100));
+it('calculates discounted price', function () {
+    $dicountedPrice = $this->meal->price - round(($this->meal->price * ($this->meal->discount / 100)));
     $response = $this->getJson(route('meal.index'));
 
     $this->assertEquals($dicountedPrice, $this->meal->discounted_price);
