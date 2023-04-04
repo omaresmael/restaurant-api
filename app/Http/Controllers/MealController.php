e3f4821ba19fb2d4cec93cbdc6932b415837e8f9
omaresmaeel <omaresmael590@gmail.com>
2023-04-04 19:31:08 +0200

    specify the returned columns

--- app/Http/Controllers/MealController.php
+++ app/Http/Controllers/MealController.php
@@ -10,6 +10,10 @@ class MealController extends Controller
 {
     public function index(): MealCollection
     {
-        return new MealCollection(Meal::cursorPaginate(Meal::PAGINATION_COUNT));
+        $meals = Meal::query()
+            ->select('id', 'name', 'description', 'available_quantity', 'price', 'discount')
+            ->cursorPaginate(Meal::PAGINATION_COUNT);
+
+        return new MealCollection($meals);
     }
 }
