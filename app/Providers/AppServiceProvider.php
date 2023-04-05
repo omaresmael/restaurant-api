8ce59ebc1c24bef1a5a8c652900e728fce993d08
omaresmaeel <omaresmael590@gmail.com>
2023-04-05 20:40:27 +0200

    fix index issues

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Services\Invoicable;
 use App\Services\InvoiceService;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,6 +17,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
