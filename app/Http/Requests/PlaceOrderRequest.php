6b2c6f64721f50f572fe6c311dd33a73d7b09d78
omaresmaeel <omaresmael590@gmail.com>
2023-04-07 17:30:07 +0200

    prevent placing orders with invalid meals

--- app/Http/Requests/PlaceOrderRequest.php
+++ app/Http/Requests/PlaceOrderRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Database\Query\Builder;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class PlaceOrderRequest extends FormRequest
 {
@@ -16,7 +18,9 @@ class PlaceOrderRequest extends FormRequest
         return [
             'reservation_id' => ['required', 'integer', 'exists:reservations,id'],
             'meals' => ['required', 'array'],
-            'meals.*.meal_id' => ['required', 'integer', 'exists:meals,id'],
+            'meals.*.meal_id' => ['required', 'integer', Rule::exists('meals','id')->where(function (Builder $query) {
+                return $query->where('available_quantity', '>', 0);
+            })],
             'meals.*.amount_to_pay' => ['required', 'numeric', 'min:0'],
             'waiter_id' => ['required', 'integer'],
         ];
